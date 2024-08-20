1623d5e26fa26b1c5de1194d4c66b800d2c80563
Sushil Singh <fullstacksushil@gmail.com>
2024-08-20 09:54:02 +0530

    i3

---
+++ X11/monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --off --output DP-1 --off --output DP-2 --off --output HDMI-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2-8 --mode 2560x1440 --pos 640x2160 --rotate normal --output DP-2-1 --off
